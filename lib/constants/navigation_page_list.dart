f5f2816997051e05b5057c6a21cc956e7d9aa608
Shahin Abdolmaleki <shahinmaleki71@gmail.com>
2024-07-09 17:25:04 +0330

    homepage

--- lib/constants/navigation_page_list.dart
+++ lib/constants/navigation_page_list.dart
@@ -2,17 +2,17 @@ import 'package:fintech/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 final List<Widget> bodyContent = [
-    const HomePage(),
-    const Text(
-      "Notification Page",
-    ),
-    const Text(
-      "Message Page",
-    ),
-    const Text(
-      "Message Page",
-    ),
-    const Text(
-      "Message Page",
-    ),
-  ];
+  const HomePage(),
+  const Text(
+    "Page2",
+  ),
+  const Text(
+    "Page3",
+  ),
+  const Text(
+    "Page3",
+  ),
+  const Text(
+    "Page4",
+  ),
+];
